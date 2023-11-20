0da88dfb4e3cfdc31506efc245a40f8c0a3881ad
MilindShekhawat <milindshekhawat10@gmail.com>
2023-11-20 18:19:07 +0530

    Created a sidebar component.

---
+++ app/components/Sidebar.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function Sidebar() {
+  return (
+    <div>
+      <div className='bg-neutral-300 w-64 h-screen overflow-y-scroll'>Hello</div>
+    </div>
+  )
+}
