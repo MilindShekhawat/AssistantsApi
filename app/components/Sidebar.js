ac79e5bd3ef913099f76854c3e023fe34ad878cf
MilindShekhawat <milindshekhawat10@gmail.com>
2023-11-21 15:30:39 +0530

    Added Links to assistants in SideBar.

--- app/components/Sidebar.js
+++ app/components/Sidebar.js
@@ -1,7 +1,8 @@
 "use client"
 import { useState } from "react"
 import { selectAssistant } from "../features/assistants/assistantSlice"
-import { useDispatch } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
+import Link from "next/link"
 
 export default function Sidebar(props) {
   const dispatch = useDispatch()
@@ -19,12 +20,13 @@ export default function Sidebar(props) {
       scrollbar-thin scrollbar-track-neutral-800 scrollbar-thumb-rounded-full hover:scrollbar-thumb-neutral-600'>
         <div className='font-bold text-xl text-neutral-200 pl-5 p-3 mb-2 border-b border-neutral-800'>Assistants</div>
         {props.assistants.map((assistant) => (
-          <div
+          <Link
+            href={`/${assistant.id}`}
             key={assistant.name}
             onMouseEnter={() => displayAssistant(assistant)}
             className={`flex pl-4 ml-1 mr-2 p-2 rounded-3xl ${activeBot === assistant ? "bg-neutral-800" : ""}`}>
             <span className='font-bold text-neutral-200'>{assistant.name}</span>
-          </div>
+          </Link>
         ))}
       </div>
     </div>
