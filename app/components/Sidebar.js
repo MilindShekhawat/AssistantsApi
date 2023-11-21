48f11f981ae7c7f37021388093208a00a960ee38
MilindShekhawat <milindshekhawat10@gmail.com>
2023-11-21 12:52:50 +0530

    Added useDispatch to SideBar.

--- app/components/Sidebar.js
+++ app/components/Sidebar.js
@@ -1,6 +1,13 @@
-import React from "react"
+"use client"
+import { selectAssistant } from "../features/assistants/assistantSlice"
+import { useDispatch } from "react-redux"
 
 export default function Sidebar(props) {
+  const dispatch = useDispatch()
+  function displayAssistant(assistant) {
+    console.log(assistant)
+    dispatch(selectAssistant(assistant))
+  }
   return (
     <div>
       <div
@@ -10,7 +17,8 @@ export default function Sidebar(props) {
         {props.assistants.map((assistant) => (
           <div
             key={assistant.name}
-            className='flex pl-4 ml-1 mr-2 p-2 rounded-2xl hover:bg-neutral-900'>
+            onClick={() => displayAssistant(assistant)}
+            className='flex pl-4 ml-1 mr-2 p-2 rounded-3xl hover:bg-neutral-900'>
             <span className='font-bold text-neutral-200'>{assistant.name}</span>
             {/* <span className='font-medium text-sm text-neutral-600 pt-1'>{assistant.description}</span> */}
           </div>
