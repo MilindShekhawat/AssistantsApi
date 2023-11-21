202c4f4452c01631524ffd992f555afaaf071d15
MilindShekhawat <milindshekhawat10@gmail.com>
2023-11-21 18:07:10 +0530

    minor changes.

--- app/components/ThreadsSidebar.js
+++ app/components/ThreadsSidebar.js
@@ -7,12 +7,11 @@ import Link from "next/link"
 export default function ThreadsSidebar(props) {
   const [activeItem, setActiveItem] = useState(null)
   return (
-    <div className='flex flex-col w-48 bg-black h-screen border-r border-neutral-800'>
-      <div className='font-bold h-14 text-xl text-neutral-200 pl-5 p-3 mb-2 border-b border-neutral-800'></div>
+    <div className='flex flex-col w-64 bg-black h-screen border-r border-neutral-800'>
+      <div className='font-bold h-14 text-xl text-neutral-200 pl-5 p-3 mb-2 border-b border-neutral-800'>{props.assistant.name}</div>
       <div
-        className={`overflow-y-scroll 
-        scrollbar-thin scrollbar-track-neutral-800 scrollbar-thumb-rounded-full hover:scrollbar-thumb-neutral-600`}></div>
-      {/* <button onClick={() => setCollapsedSidebar(!collapsedSidebar)}>Click</button> */}
+        className={`overflow-y-scroll scrollbar-thin scrollbar-track-neutral-800 scrollbar-thumb-rounded-full
+        hover:scrollbar-thumb-neutral-600`}></div>
     </div>
   )
 }
