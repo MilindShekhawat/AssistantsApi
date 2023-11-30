4dcf57f02a5510c8b923a099fa8d3b14b423570e
MilindShekhawat <milindshekhawat10@gmail.com>
2023-11-30 17:26:36 +0530

    Improved Redux structuring. Removed redux funtionality for now.

--- app/store/store.js
+++ app/store/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit"
-import listAssistants from "./features/assistants/assistantSlice"
-import selectAssistant from "./features/assistants/assistantSlice"
+import listAssistants from "../features/assistants/assistantSlice"
+import selectAssistant from "../features/assistants/assistantSlice"
 
 export const store = configureStore({
   reducer: listAssistants,
