4f86331fe6d87545808a5534e30c6274c7854c69
MilindShekhawat <milindshekhawat10@gmail.com>
2023-12-04 18:30:36 +0530

    feat: Added a create thread button.

--- app/components/ThreadsSidebar.js
+++ app/components/ThreadsSidebar.js
@@ -1,12 +1,21 @@
 "use client"
 import Link from "next/link"
+import { CreateThread } from "../services/apiendpoints"
 
 //ThreadSidebar, shows all the threads present in an assistant
 export default function ThreadsSidebar(props) {
+  const createThread = async () => {
+    const newthread = await CreateThread(props.assistant.id, props.userId)
+    console.log("New Thread", newthread.id)
+  }
+
   return (
     <div className='relative flex flex-col w-64 h-screen bg-black border-r border-neutral-800'>
-      <div className='flex justify-between flex-shrink-0 gap-2 p-3 pl-5 text-xl font-bold border-b h-14 text-neutral-200 border-neutral-800'>
-        <span>{props.assistant.name}</span>
+      <div className='flex justify-between flex-shrink-0 gap-2 p-3 pl-5 font-bold border-b h-14 text-neutral-200 border-neutral-800'>
+        <span className='text-xl truncate'>{props.assistant.name}</span>
+        <button onClick={() => createThread()} className=''>
+          Create
+        </button>
       </div>
       <div className='h-full overflow-y-scroll scrollbar-thin scrollbar-thumb-neutral-800 scrollbar-thumb-rounded-full hover:scrollbar-thumb-neutral-600'>
         {props.threads.map((thread) => (
