3d61088660e1d601f7a8539b9b613c511c57becc
MilindShekhawat <milindshekhawat10@gmail.com>
2023-12-04 18:29:53 +0530

    fix: Added an intermediate page

---
+++ app/[assistantId]/page.js
@@ -0,0 +1,20 @@
+import { GetThreads, CreateThread } from "@/app/services/apiendpoints"
+import { auth } from "@clerk/nextjs"
+import { redirect } from "next/navigation"
+
+//Assistant Page where we can chat with that specific assistant
+export default async function ThreadsPage({ params }) {
+  //Calling apis here and will prop drill them to components
+  const { userId } = auth()
+  const threads = await GetThreads(params.assistantId, userId)
+
+  if (threads.length == 0) {
+    const currentThread = await CreateThread(params.assistantId, userId)
+    redirect(`/${params.assistantId}/${currentThread.id}`)
+  } else {
+    const currentThread = threads[0]
+    redirect(`/${params.assistantId}/${currentThread.threadId}`)
+  }
+
+  return <main className='relative flex'>Loading</main>
+}
