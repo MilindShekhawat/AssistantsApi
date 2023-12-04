e1b6a5c245e2781b1ae00e76726fe9ddd5448eea
MilindShekhawat <milindshekhawat10@gmail.com>
2023-12-04 17:32:03 +0530

    feat: Added a signout button.

--- app/components/Sidebar.js
+++ app/components/Sidebar.js
@@ -1,12 +1,14 @@
 "use client"
 import { useState } from "react"
 import Link from "next/link"
+import { useUser, UserButton } from "@clerk/nextjs"
 
 //Sidebar, contains all of the available assistants
 export default function Sidebar(props) {
   //Sidebar states for assistant selection and if the sidebar is collapsed or not
   const [activeItem, setActiveItem] = useState(null)
   const collapsedSidebar = props.isCollapsed
+  const { user, isLoaded } = useUser()
 
   return (
     <div className='relative flex flex-col h-screen bg-black border-r border-neutral-800'>
@@ -20,7 +22,7 @@ export default function Sidebar(props) {
           <div key={assistant.name}>
             <Link
               className={`flex h-11 mt-2 rounded-3xl overflow-hidden m-1.5
-              ${collapsedSidebar == true ? "bg-neutral-800" : "pl-5 p-2"} 
+              ${collapsedSidebar == true ? "bg-neutral-800" : "pl-5 p-2.5"} 
               ${activeItem === assistant ? "bg-neutral-800" : ""}`}
               //TODO add logic to switch to first threadid present in assistant
               //TODO create a new thread in no threads are present
@@ -36,8 +38,10 @@ export default function Sidebar(props) {
           </div>
         ))}
       </div>
+      <div className='flex-shrink-0 h-[66.6px] border-t-[1px] border-neutral-800 flex p-4 items-center'>
+        {isLoaded && user && <UserButton afterSignOutUrl='/' />}
+      </div>
       {/* LINES FOR DESIGN */}
-      <div className='flex-shrink-0 h-[66.6px] border-t-[1px] border-neutral-800 flex p-3 items-center'></div>
       <div className='absolute right-2 top-14 bottom-[66.6px] h-auto border-l-[1px] border-neutral-800'></div>
     </div>
   )
