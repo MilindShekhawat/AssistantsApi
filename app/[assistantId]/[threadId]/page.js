445e39a7514cff8b2f382f9cebb0ae82e2df98a2
MilindShekhawat <milindshekhawat10@gmail.com>
2023-12-04 12:32:08 +0530

    feat: First time page load displays old messages.

--- app/[assistantId]/[threadId]/page.js
+++ app/[assistantId]/[threadId]/page.js
@@ -1,21 +1,22 @@
 import ThreadsSidebar from "@/app/components/ThreadsSidebar"
 import MessageWindow from "@/app/components/MessageWindow"
 import Sidebar from "@/app/components/Sidebar"
-import { GetAssistant, GetAssistants, GetThreads } from "@/app/services/apiendpoints"
+import { GetAssistant, GetAssistants, GetThreads, GetMessages } from "@/app/services/apiendpoints"
 
 //* thread_y6fQawHZgGz0qyQRgL35PGes
 //Assistant Page where we can chat with that specific assistant
 export default async function ThreadsPage({ params }) {
   //Calling apis here and will prop drill them to components
-  const myAssistant = await GetAssistant(params.assistantId)
   const myAssistants = await GetAssistants()
+  const myAssistant = await GetAssistant(params.assistantId)
   const threads = await GetThreads(params.assistantId)
+  const messageList = await GetMessages(params.threadId)
 
   return (
     <main className='relative flex'>
       <Sidebar assistants={myAssistants.data} isCollapsed={true} />
       <ThreadsSidebar assistant={myAssistant} threads={threads} />
-      <MessageWindow assistantId={params.assistantId} threadId={params.threadId} />
+      <MessageWindow assistantId={params.assistantId} threadId={params.threadId} messageList={messageList} />
       {/* LINES FOR DESIGN */}
       <hr className='absolute w-full border-neutral-800 top-14' />
       <hr className='absolute w-full border-neutral-800 bottom-[65.6px]' />
