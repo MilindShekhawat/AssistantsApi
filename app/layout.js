cf3cebda1f698f6bba7fcb9fdb431463e5383ade
MilindShekhawat <milindshekhawat10@gmail.com>
2023-12-05 13:07:21 +0530

    style: Added darkmode to clerk ui.

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google"
 import "./globals.css"
 import StoreProvider from "./store/StoreProvider"
 import { ClerkProvider } from "@clerk/nextjs"
+import { dark } from "@clerk/themes"
 import { GeistSans } from "geist/font/sans"
 import { GeistMono } from "geist/font/mono"
 
@@ -14,7 +15,10 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+      }}>
       <StoreProvider>
         <html lang='en' className={GeistSans.className}>
           <body>{children}</body>
